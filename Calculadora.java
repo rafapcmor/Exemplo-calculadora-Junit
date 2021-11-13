3a3109b7296caa184ece0f28247c5e984a92dba7
Rafaela Moreira <45953979+rafapcmor@users.noreply.github.com>
2021-11-13 11:41:06 -0300

    Classe Calculadora e CalculadoraTest

---
+++ Calculadora.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.teste;
+
+/**
+ *
+ * @author rafap
+ */
+public class Calculadora {
+
+    public double somar(double a, double b) {
+        return a + b;
+    }
+
+    public double subtrair(double a, double b) {
+        return a - b;
+    }
+
+    public double multiplicar(double a, double b) {
+        return a * b;
+    }
+
+    public double dividir(double a, double b) {
+        if(b==0){
+           throw new IllegalArgumentException("Divisão por 0 não é permitido!");
+        }    
+        return a / b;
+    }
+
+}
